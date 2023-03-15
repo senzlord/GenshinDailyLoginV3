1168000f36b8bb778e0f5b60dfc3c6b426d27fb8
Hasyidan Paramananda <22251174+senzlord@users.noreply.github.com>
2023-03-16 02:55:15 +0700

    Update main.py
    - add validation on Database Connected!
    - change timestamp to direct datetime.now()

--- GetLogin/main.py
+++ GetLogin/main.py
@@ -13,7 +13,14 @@ load_dotenv()
 mongo_client_link = os.getenv('MONGO_CLIENT_LINK')
 mongo_client_db = os.getenv('MONGO_CLIENT_DB')
 mongo_client_collection = os.getenv('MONGO_CLIENT_COLLECTION')
-mongo_client = pymongo.MongoClient(mongo_client_link)
+try:
+    mongo_client = pymongo.MongoClient(mongo_client_link)
+    mongo_client.server_info()
+    print("Database connected!")
+except Exception as err:
+    print("Database connected error!")
+    exit()
+
 manual_timer = 80
 
 # Get Username & Password
@@ -28,14 +35,13 @@ users_collection = hoyolab_db[mongo_client_collection]
 
 # check if user with entered username already exists in the database
 existing_user = users_collection.find_one({"username": username})
-timestamp = datetime.now()
 if existing_user:
     # update the existing user's cookies
-    users_collection.update_one({"_id": existing_user["_id"]}, {"$set": {"cookies": hoyolab_cookies, 'timestamp': timestamp}})
+    users_collection.update_one({"_id": existing_user["_id"]}, {"$set": {"cookies": hoyolab_cookies, 'timestamp': datetime.now()}})
     print(f"User {username}'s cookies updated in MongoDB")
 else:
     # insert a new user with the entered username and extracted cookies
-    user_data = {"username": username, "cookies": hoyolab_cookies, 'timestamp': timestamp}
+    user_data = {"username": username, "cookies": hoyolab_cookies, 'timestamp': datetime.now()}
     users_collection.insert_one(user_data)
     print(f"New user {username} and cookies saved to MongoDB")
 
